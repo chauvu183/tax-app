4a2941e4fb1471295f737fcc3b692a387e56a51c
chauvu183 <kiwill.vu@gmail.com>
2023-09-03 19:05:42 +0200

    create a simple form to get user input

--- src/app/receipt/receipt.component.ts
+++ src/app/receipt/receipt.component.ts
@@ -1,20 +1,26 @@
 import {Component} from '@angular/core';
+import {Item} from "../models/item";
+
+interface TaxItemList{
+  items: any[], salesTaxes: number, total: number
+}
 
 @Component({
   selector: 'app-receipt',
   templateUrl: './receipt.component.html',
   styleUrls: ['./receipt.component.scss']
 })
+
 export class ReceiptComponent {
   basicTaxRate = 10;
   importedTaxRate = 5;
+  taxedItemsList :Item[] = [];
+  salesTaxes = 0;
+  totalPrice = 0;
 
-  calculateTax(input: any[]): { items: any[], salesTaxes: number, total: number } {
+  calculateTax(input: any[]): TaxItemList {
     const itemsList:any[] = [];
-    let salesTaxes = 0;
-    let totalPrice = 0;
-
-    return {items: itemsList, salesTaxes, total: totalPrice};
+    return {items: itemsList, salesTaxes: this.salesTaxes, total: this.totalPrice};
   }
 
   roundUp(price: number): number {
