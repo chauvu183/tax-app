d597db86f6bfc3901b327b5c05d86ed4fa4912af
chauvu183 <kiwill.vu@gmail.com>
2023-09-03 18:04:43 +0200

    create receipt component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReceiptComponent } from './receipt/receipt.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReceiptComponent
   ],
   imports: [
     BrowserModule,
